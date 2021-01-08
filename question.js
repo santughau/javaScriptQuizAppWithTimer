32ca6315d0d521c4325d88d87f194e924de5ecae
santughau <santu.ghau@gmail.com>
2021-01-08 07:45:04 +0530

    first commit

---
+++ question.js
@@ -0,0 +1,32 @@
+let questions = [
+    {
+        numb: 1,
+        question: "  Which of the following is not JavaScript Data Types?",
+        answer: "Float",
+        options: ['Undefined value', 'Number', 'Boolean', 'Float']
+    },
+    {
+        numb: 2,
+        question: " Which company developed JavaScript?",
+        answer: "Netscape",
+        options: ['Netscape', 'Bell Labs', 'Sun Microsystems', 'IBM']
+    },
+    {
+        numb: 3,
+        question: "  Inside which HTML element do we put the JavaScript?",
+        answer: "script",
+        options: ['script', ' head', 'meta', 'style']
+    },
+    {
+        numb: 4,
+        question: "  Which of the following is correct about features of JavaScript?",
+        answer: "JavaScript is a object-based scripting language.",
+        options: ['It can not Handling dates and time.', 'JavaScript is a object-based scripting language.', 'JavaScript is not interpreter based scripting language.', 'All of the above']
+    },
+    {
+        numb: 5,
+        question: "  Which of the following is not Javascript frameworks or libraries?",
+        answer: "Cassandra",
+        options: ['Polymer', 'Meteor', 'Cassandra', ' jQuery']
+    },
+];
